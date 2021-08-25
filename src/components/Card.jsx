abb968ab1247be3625f0879dd9baa65f5a01738e
Alex Nweke <nwekealexchinedu@gmail.com>
2021-08-25 05:03:57 +0100

    added the shorten of the card title functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, Dimensions } from 'react-native';
 // icons
 import { Entypo, Ionicons, AntDesign, MaterialIcons } from '@expo/vector-icons';
 
 const Card = () => {
   return (
-    <View>
+    <View style={styles?.container}>
       <Image
         source={{
           uri: 'https://images.unsplash.com/photo-1496715976403-7e36dc43f17b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
         }}
         style={styles?.imgStyle}
       />
-      <View style={styles?.row}>
-        <View></View>
+      <View style={[styles?.row, styles?.m5]}>
         <View>
-          <Text>This the name of the Video</Text>
+          <MaterialIcons name='account-circle' size={32} color='gray' />
+        </View>
+        <View style={styles?.ml10}>
+          <Text style={styles?.title} ellipsizeMode='tail' numberOfLines={2}>
+            This the name of the Video, but the name of this video is very very very long so we can
+            test the shorten of the text with react native
+          </Text>
           <Text>This the name of the channel</Text>
         </View>
       </View>
@@ -24,18 +29,29 @@ const Card = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+  container: {},
   imgStyle: {
     width: '100%',
     height: 200,
   },
   row: {
     flexDirection: 'row',
-    justifyContent: 'center',
+    // justifyContent: 'center',
     alignContent: 'center',
     alignItems: 'center',
+  },
+  m5: {
+    margin: 5,
+  },
+  ml5: {
+    marginLeft: 5,
+  },
+  ml10: {
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 20,
+    width: Dimensions.get('screen').width - 50,
   },
 });
 
