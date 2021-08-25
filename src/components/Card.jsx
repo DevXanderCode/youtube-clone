6451fe10b3e14abb9fb7f5412be943717199bdc3
Alex Nweke <nwekealexchinedu@gmail.com>
2021-08-25 04:47:41 +0100

    displayed the card in the home screen component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,19 @@ import { Entypo, Ionicons, AntDesign, MaterialIcons } from '@expo/vector-icons';
 const Card = () => {
   return (
     <View>
-      <Text>Hello from the card component</Text>
+      <Image
+        source={{
+          uri: 'https://images.unsplash.com/photo-1496715976403-7e36dc43f17b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        }}
+        style={styles?.imgStyle}
+      />
+      <View style={styles?.row}>
+        <View></View>
+        <View>
+          <Text>This the name of the Video</Text>
+          <Text>This the name of the channel</Text>
+        </View>
+      </View>
     </View>
   );
 };
@@ -14,6 +26,16 @@ const Card = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  imgStyle: {
+    width: '100%',
+    height: 200,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
   },
 });
 
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+// layout
 import Header from '../components/Header';
+import Card from '../components/Card';
 
 const HomeScreen = () => {
   return (
     <View style={styles?.container}>
       <Header />
+      <Card />
       <Text>Hello from the Home Component</Text>
     </View>
   );
