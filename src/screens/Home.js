e1406c0d4e15ec642bdf36a79ccc542b26ac9bb0
Alex Nweke <nwekealexchinedu@gmail.com>
2021-08-25 03:46:45 +0100

    created the folder structure and started working on the home screen

---
+++ src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={styles?.container}>
+      <Text>Hello from the Home Component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default Home;
